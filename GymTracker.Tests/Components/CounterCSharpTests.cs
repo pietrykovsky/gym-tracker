a96417e1a1afcc4afdab4ea517b1c996ffb375b3
Michał Pietrykowski <93202530+pietrykovsky@users.noreply.github.com>
2024-10-31 00:32:29 +0100

    Implement GitHub Actions CI/CD Pipeline for Automated Testing and Code Quality (#8)
    * Add ci github action workflow. Modify dotnet static analysis configuration
    
    * Update formatting
    
    * Fix permissions issue
    
    * Fix syntax issue

--- GymTracker.Tests/Components/CounterCSharpTests.cs
+++ GymTracker.Tests/Components/CounterCSharpTests.cs
@@ -6,26 +6,26 @@ namespace GymTracker.Tests.Components;
 /// </summary>
 public class CounterCSharpTests : TestContext
 {
-	[Fact]
-	public void CounterStartsAtZero()
-	{
-		// Arrange
-		var cut = RenderComponent<Counter>();
+    [Fact]
+    public void CounterStartsAtZero()
+    {
+        // Arrange
+        var cut = RenderComponent<Counter>();
 
-		// Assert that content of the paragraph shows counter at zero
-		cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
-	}
+        // Assert that content of the paragraph shows counter at zero
+        cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
+    }
 
-	[Fact]
-	public void ClickingButtonIncrementsCounter()
-	{
-		// Arrange
-		var cut = RenderComponent<Counter>();
+    [Fact]
+    public void ClickingButtonIncrementsCounter()
+    {
+        // Arrange
+        var cut = RenderComponent<Counter>();
 
-		// Act - click button to increment counter
-		cut.Find("button").Click();
+        // Act - click button to increment counter
+        cut.Find("button").Click();
 
-		// Assert that the counter was incremented
-		cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
-	}
+        // Assert that the counter was incremented
+        cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+    }
 }
